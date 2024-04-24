c9bf01f82f1c393b2be855da5ba56289cfa3d38a
elusojitimileyin <elusojitimileyin@gmail.com>
2024-04-24 17:13:28 +0100

    solution updated

--- ReadMe.js
+++ ReadMe.js
@@ -1,5 +1,7 @@
 // // Assessment 1
 
+const { forOfStatement } = require("@babel/types");
+
 function testScoresArray(numbers){
 
     return numbers.filter(num=>num >= 70);
@@ -18,7 +20,45 @@ function squareNumber(numbers) {
     return numbers.map(num => num * num);
 }
 
-module.exports = {testScoresArray, upGradeScore, squareNumber}
+
 
 // Assessment 4
+
+
+
+function shareBooks(listOfMembers){
+ 
+    for (const key of listOfMembers) {
+        key.book += key.name + "'s book"
+                }
+        }
+
+//  Assessment 5
+
+
+function davidMonthlyExpenses(expenses){
+let result = 0;
+
+    for (const category in expenses){
+    result += expenses[category]
+    }
+    return result;
+}
+
+function filterAfternoonTime(time) {
+    if (time === "12:00 PM" || time === "1:00 PM" || time === "2:00 PM" || time === "3:00 PM" || time === "4:00 PM" || time === "5:00 PM"){
+        return time
+
+    }}
+    function scheduleAfternoonClasses(classTimings) {
+        return classTimings.filter(time => filterAfternoonTime(time))
+    }
+
+
+    function mapHealthyAndUnHealthy(array) {
+
+
+    }
+
+module.exports = {testScoresArray, upGradeScore, squareNumber,shareBooks,davidMonthlyExpenses,scheduleAfternoonClasses} 
 
--- ReadMe.test.js
+++ ReadMe.test.js
@@ -1,4 +1,4 @@
-let {testScoresArray, upGradeScore,squareNumber} = require("./ReadMe.js")
+let {testScoresArray, upGradeScore,squareNumber,shareBooks,davidMonthlyExpenses,scheduleAfternoonClasses} = require("./ReadMe.js")
 
 test("filter to check testscores  ", () =>{
     let score = [85, 92, 78, 88, 95, 50, 20, 40, 34, 70];
@@ -16,4 +16,49 @@ test("map to square numbers  ", () =>{
     let input = [2,4,6,8,10];
     let answer = squareNumber(input);
     expect(answer).toEqual([ 4, 16, 36, 64, 100 ]) 
+})
+
+test("list of books ", () =>{
+
+    const member1= {
+        name: "Emily",
+        book : ""
+    }
+    
+    const member2 = {
+        name: "Jack",
+        book : ""
+        
+    }
+    const member3 = {
+        name: "Sophia",
+        book: ""
+    };
+    
+    const member4= {
+        name: "Daniel",
+        book : ""
+    };
+
+    const readers  = [member1, member2, member3, member4]; 
+    shareBooks(readers);
+    expect(readers[1].book).toBe("Jack's book") 
+})
+
+
+
+test("calculate the total amount of groceries spent ", () =>{
+
+    let expenses ={ "groceries": 150, "dining out": 100, "transportation": 50, "entertainment": 80 }
+    let result = 380
+    let answer = davidMonthlyExpenses(expenses)
+    expect(answer).toEqual(result) 
+})
+
+
+test(" test for classes offered in the afternoon ", () =>{
+    let classTime = ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM"];
+    let result = ["1:00 PM", "3:00 PM", "5:00 PM"]
+    let answer = scheduleAfternoonClasses(classTime);
+    expect(answer).toEqual(result) 
 })
